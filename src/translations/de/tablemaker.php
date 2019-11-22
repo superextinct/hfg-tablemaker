52b278d2d57b45890a2fae92f466ea430e3e6305
superextinct <desk@niklassonnenschein.de>
2019-11-22 13:05:49 +0100

    German translation

---
+++ src/translations/de/tablemaker.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Table Maker plugin for Craft CMS 3.x
+ *
+ * A user-definable table field type for Craft CMS
+ *
+ * @link      http://www.supercooldesign.co.uk/
+ * @copyright Copyright (c) 2018 Supercool Ltd
+ */
+
+/**
+ * Table Maker de Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('tablemaker', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Niklas Sonnenschein
+ * @package   TableMaker
+ * @since     2.0.2
+ */
+
+return [
+    'Table Maker plugin loaded' => 'Table Maker Plugin geladen',
+    'Heading' => 'Überschrift',
+    'Width' => 'Zeilenbreite',
+    'Alignment' => 'Ausrichtung',
+    'Left' => 'Links',
+    'Center' => 'Zentriert',
+    'Right' => 'Rechts',
+    'Type' => 'Typ',
+    'Date' => 'Datum'
+];
